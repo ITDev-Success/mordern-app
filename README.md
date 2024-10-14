1ec96fe7c28e8ed72af654b025e3a778e89d53a8
TSIDIANY Raharison Muriel <github@itdevsuccess.com>
2024-10-14 16:27:47 +0300

    Finiliser l'apk

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.themoviedb.org/movie/533535-deadpool-wolverine#play=Idh8n5XuYIA
+
+https://www.youtube.com/watch?v=tbrJJkSYQ04
--- src/app/home-defer/home-defer.page.ts
+++ src/app/home-defer/home-defer.page.ts
@@ -1,20 +1,95 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  InfiniteScrollCustomEvent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonSkeletonText,
+  IonAlert,
+  IonLabel,
+  IonText,
+  IonBadge,
+  IonInfiniteScroll,IonInfiniteScrollContent, IonSpinner } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interfaces';
+import { environment } from 'src/environments/environment.prod';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-defer',
-  templateUrl: './home-defer.page.html',
-  styleUrls: ['./home-defer.page.scss'],
+  templateUrl: 'home-defer.page.html',
+  styleUrls: ['home-defer.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonSpinner, 
+    IonInfiniteScrollContent,
+    IonInfiniteScroll,
+    IonBadge,
+    IonText,
+    IonLabel,
+    IonAlert,
+    IonSkeletonText,
+    IonAvatar,
+    IonItem,
+    IonList,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    DatePipe,
+    RouterLink,
+  ],
 })
-export class HomeDeferPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeDeferPage {
+  private movieService = inject(MovieService);
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public movies: MovieResult[] = [];
+  public dummyArray = new Array(5);
+  public imageBaseUrl = environment.IMAGE_BASE;
+  constructor() {
+    this.loadMovies();
   }
 
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+    if (!event) {
+      this.isLoading = true;
+    }
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+            // event.target.disabled
+          }
+        }),
+        catchError((e: any) => {
+          this.error = e.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          this.movies.push(...res.results);
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+      });
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage ++
+
+    this.loadMovies(event)
+  }
 }
